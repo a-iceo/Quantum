1b5c4cc11a64102049557d646bee541b45985394
a-iceo <125764183+a-iceo@users.noreply.github.com>
2025-03-14 12:01:29 -0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Quantum',
+  assetPrefix: '/Quantum/',
+  experimental: {
+    esmExternals: 'loose'
+  },
+  webpack: (config) => {
+    config.externals = [...config.externals, { canvas: "canvas" }]; // required to make pdfjs work
+    return config;
+  },
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
